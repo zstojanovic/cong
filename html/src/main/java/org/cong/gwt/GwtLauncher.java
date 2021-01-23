98cb12af267327a2e730035a345e4aeb65ddae3d
zstojanovic <zoran.stojanovic@inbox.com>
2021-01-23 16:44:00 +0100

    Tweak preload logo

--- html/src/main/java/org/cong/gwt/GwtLauncher.java
+++ html/src/main/java/org/cong/gwt/GwtLauncher.java
@@ -3,7 +3,11 @@ package org.cong.gwt;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
 import com.github.czyzby.websocket.GwtWebSockets;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.Panel;
 import org.cong.client.Game;
 
 public class GwtLauncher extends GwtApplication {
@@ -14,6 +18,19 @@ public class GwtLauncher extends GwtApplication {
     config.useAccelerometer = false;
     config.antialiasing = true;
     return config;
+  }
+
+  @Override
+  public Preloader.PreloaderCallback getPreloaderCallback() {
+    return createPreloaderPanel(GWT.getHostPageBaseURL() + "preloadlogo.png");
+  }
+
+  @Override
+  protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+    meterPanel.setStyleName("gdx-meter");
+    meterPanel.addStyleName("nostripes");
+    meterStyle.setProperty("backgroundColor", "#e3c615");
+    meterStyle.setProperty("backgroundImage", "none");
   }
 
   @Override
